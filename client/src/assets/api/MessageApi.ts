4f14a1d33928814312b50ea8861b16c080d1df56
Girlie Razon <girlie.razon84@gmail.com>
2025-09-11 23:16:14 +0200

    Add MessageApi module for Axios instance configuration

---
+++ client/src/assets/api/MessageApi.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const MessageApi = axios.create({
+    // baseURL: "http://localhost:5000/api", // ✅ full backend URL- works for local dev
+    baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api", // ✅ works for both local dev and deployed frontend
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export default MessageApi;
