853ef25781a8902c96778b0a52e377a5542f195b
Girlie Razon <girlie.razon84@gmail.com>
2025-09-13 18:11:17 +0200

    feat: update name in Hero component for accuracy

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -127,7 +127,7 @@ const Hero: React.FC = () => {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8, delay: 0.6 }}
                 >
-                    Hi, I'm Girlie Razon 👋
+                    Hi, I'm Girlie Quindao Razon 👋
                 </NameTitle>
 
                 <Subtitle
