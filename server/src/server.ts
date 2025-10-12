d4f9624ac99219ce7d6b3f3e92713d93b3495cb4
Girlie Razon <girlie.razon84@gmail.com>
2025-10-12 18:07:05 +0200

    feat: add additional CORS origin for improved access

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,7 +22,8 @@ mongoose
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://gqr-portfolio.vercel.app"
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
