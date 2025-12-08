ca451bd06891799556d73dabe0f46d0dd790f2c7
Girlie Razon <girlie.razon84@gmail.com>
2025-12-08 04:27:47 +0100

    feat: add TrichMindPage component to render TrichMind project section

---
+++ client/src/app/projects/trichmind/page.tsx
@@ -0,0 +1,13 @@
+// client/src/app/projects/trichmind/page.tsx
+"use client";
+
+import React from "react";
+import TrichMind from "../../../components/projects/TrichMind";
+
+
+// TrichMind Project Page Component
+const TrichMindPage: React.FC = () => {
+    return <TrichMind />;
+};
+
+export default TrichMindPage;
