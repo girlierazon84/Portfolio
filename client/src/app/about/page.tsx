3935b336e3ea6e0f28beeb5b47b8d9ccee66f606
Girlie Razon <girlie.razon84@gmail.com>
2025-12-08 04:27:31 +0100

    feat: add AboutPage component to render About section

---
+++ client/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+// client/src/app/about/page.tsx
+"use client";
+
+import About from "../../components/About";
+
+
+// AboutPage component that renders the About component
+export default function AboutPage() {
+    return <About />;
+}
