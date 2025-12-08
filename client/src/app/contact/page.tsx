95a7c3666cb97c65099ad3ac9b62b89bc4efd7e4
Girlie Razon <girlie.razon84@gmail.com>
2025-12-08 04:27:36 +0100

    feat: add ContactPage component to render Contact section

---
+++ client/src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+// client/src/app/contact/page.tsx
+"use client";
+
+import Contact from "../../components/Contact";
+
+
+// ContactPage component that renders the Contact component
+export default function ContactPage() {
+    return <Contact />;
+}
