903285c73a4e9b54611251fa6d4a51bfffe29d26
Girlie Razon <girlie.razon84@gmail.com>
2025-12-08 04:27:41 +0100

    feat: create ProjectsPage component to render Projects section

---
+++ client/src/app/projects/page.tsx
@@ -0,0 +1,10 @@
+// client/src/app/projects/page.tsx
+"use client";
+
+import Projects from "../../components/Project";
+
+
+// ProjectsPage component that renders the Projects component
+export default function ProjectsPage() {
+    return <Projects />;
+}
