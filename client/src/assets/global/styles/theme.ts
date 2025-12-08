595180dfaa91b227f57376507254391563f12d82
Girlie Razon <girlie.razon84@gmail.com>
2025-12-08 04:24:56 +0100

    feat: create theme file with color definitions for styling

---
+++ client/src/assets/global/styles/theme.ts
@@ -0,0 +1,20 @@
+// client/src/styles/theme.ts
+
+const theme = {
+    colors: {
+        bg: "#f9f8f4",
+        accent1: "#f3c408",
+        accent2: "#0cc4a8",
+        accent3: "#a78bfa",
+        text: "#f9fafb",
+        subtext: "#616161",
+        border: "#020617",
+        primary: "#000099",
+        secondary: "#E1D5E7",
+        thirdary: "#616161",
+        fourthly: "#E6E6E6"
+    }
+};
+
+export type Theme = typeof theme;
+export default theme;
