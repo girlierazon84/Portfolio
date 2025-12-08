45c596c62e379ab6b32ec33cbe91e7d957a18b33
Girlie Razon <girlie.razon84@gmail.com>
2025-12-08 04:27:53 +0100

    feat: add SkillsPage component to render Skills section

---
+++ client/src/app/skills/page.tsx
@@ -0,0 +1,10 @@
+// client/src/app/skills/page.tsx
+"use client";
+
+import Skills from "../../components/Skills";
+
+
+// SkillsPage component that renders the Skills component
+export default function SkillsPage() {
+    return <Skills />;
+}
