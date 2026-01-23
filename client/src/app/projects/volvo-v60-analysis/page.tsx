51ff21d4fe14c56fb8f37a8735d96e3d5bf15bf5
Girlie Razon <girlie.razon84@gmail.com>
2026-01-23 16:47:01 +0100

    feat: add Volvo V60 regression case study route and hook up project card

---
+++ client/src/app/projects/volvo-v60-analysis/page.tsx
@@ -0,0 +1,14 @@
+// client/src/app/projects/volvo-v60-analysis/page.tsx
+
+"use client";
+
+import React from "react";
+import VolvoV60Analysis from "../../../components/projects/Volvo-V60-Analysis";
+
+
+// Volvo V60 Regression Case Study Page
+const VolvoV60AnalysisPage: React.FC = () => {
+    return <VolvoV60Analysis />;
+};
+
+export default VolvoV60AnalysisPage;
