847f48f2eb225b30539c0054dd09d88f1974c3c9
Girlie Razon <girlie.razon84@gmail.com>
2026-01-23 16:35:16 +0100

    feat: add Volvo V60 multiple regression case study page with EDA and diagnostic visuals

---
+++ client/src/app/projects/volvo-v60/page.tsx
@@ -0,0 +1,14 @@
+// client/src/app/projects/volvo-v60/page.tsx
+
+"use client";
+
+import React from "react";
+import VolvoV60 from '../../../components/projects/Volvo-V60-Analysis';
+
+
+// Volvo-V60-Analysis Project Page Component
+const VolvoV60Page: React.FC = () => {
+    return <VolvoV60 />;
+};
+
+export default VolvoV60Page;
