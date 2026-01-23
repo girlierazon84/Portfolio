d05e76d9be3a4c5143bf2a400fafa2a4e8bf8466
Girlie Razon <girlie.razon84@gmail.com>
2026-01-23 03:27:01 +0100

    feat(projects): add BookFace MERN case study component

---
+++ client/src/app/projects/bookface/page.tsx
@@ -0,0 +1,14 @@
+// client/src/app/projects/bookface/page.tsx
+
+"use client";
+
+import React from "react";
+import BookFace from '../../../components/projects/BookFace';
+
+
+// BookFace Project Page Component
+const BookFacePage: React.FC = () => {
+    return <BookFace />;
+};
+
+export default BookFacePage;
